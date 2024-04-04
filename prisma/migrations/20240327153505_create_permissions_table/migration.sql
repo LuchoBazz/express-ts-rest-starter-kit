5dd30a0c5ffdd3e773d21b26778501d2a7c89ec6
Luis Miguel Báez <es.luismiguelbaez@gmail.com>
2024-04-04 13:55:02 -0500

    feat(init): Project initialization

--- prisma/migrations/20240327153505_create_permissions_table/migration.sql
+++ prisma/migrations/20240327153505_create_permissions_table/migration.sql
@@ -1,7 +1,6 @@
 -- CreateTable
 CREATE TABLE "users"."permissions" (
     "permission_id" UUID NOT NULL DEFAULT gen_random_uuid(),
-    "permission_role" VARCHAR(63) NOT NULL,
     "permission_name" VARCHAR(63) NOT NULL,
     "permission_created_at" TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "permission_updated_at" TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
@@ -9,5 +8,3 @@ CREATE TABLE "users"."permissions" (
     CONSTRAINT "permissions_pkey" PRIMARY KEY ("permission_id")
 );
 
--- AddForeignKey
-ALTER TABLE "users"."permissions" ADD CONSTRAINT "permissions_permission_role_fkey" FOREIGN KEY ("permission_role") REFERENCES "users"."roles"("role_name") ON DELETE RESTRICT ON UPDATE CASCADE;
---
+++ src/entities/users/permission.entity.ts
@@ -0,0 +1,30 @@
+import { Entity } from "../entity";
+
+export interface BaseUserPrisma {
+  permission_id: string;
+  permission_name: string;
+  permission_created_at: Date;
+  permission_updated_at: Date;
+}
+
+export interface UserBaseResponse {
+  id: string;
+  name: string;
+}
+
+export class PermissionEntity extends Entity {
+  private name: string;
+
+  constructor(name: string) {
+    super();
+    this.name = name;
+  }
+
+  public getName(): string {
+    return this.name;
+  }
+
+  public setName(value: string) {
+    this.name = value;
+  }
+}
