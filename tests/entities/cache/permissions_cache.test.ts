9ef6b20440f41475bfafbcc748b980adca627aa2
Luis Miguel Báez <es.luismiguelbaez@gmail.com>
2024-04-04 13:24:23 -0500

    feat(init): Project initialization

--- tests/entities/cache/permissions_cache.test.ts
+++ tests/entities/cache/permissions_cache.test.ts
@@ -1,21 +1,21 @@
 import { RoleCachePermissions } from "../../../src/entities/cache/permission_cache.entity";
 import { UserRole } from "../../../src/entities/users/role.enum";
 
-describe("RoleCachePermissions", () => {
+describe("Given a RoleCachePermissions instance", () => {
   const role = UserRole.COMMON_USER;
   const cachePermissions = new RoleCachePermissions(role);
 
-  it("generates correct key", () => {
+  it("should generates correct key", () => {
     const key = cachePermissions.generateKey();
     expect(key).toBe(role);
   });
 
-  it("returns correct search values", () => {
+  it("should returns correct search values", () => {
     const searchValues = cachePermissions.getSearchValues();
     expect(searchValues).toEqual([role]);
   });
 
-  it("returns correct role", () => {
+  it("should returns correct role", () => {
     const returnedRole = cachePermissions.getRole();
     expect(returnedRole).toBe(role);
   });
