5dd77449fefe035fc3ca9defe0f83b7bb5af2950
Luis Miguel Báez <es.luismiguelbaez@gmail.com>
2024-04-04 10:38:03 -0500

    feat(init): Project initialization

--- src/presenters/users/common_user.presenter.ts
+++ src/presenters/users/common_user.presenter.ts
@@ -1,19 +1,16 @@
 import { CommonUserEntity, UserResponse } from "../../entities/users/common_user.entity";
+import { presentUserBase } from "./user_base.presenter";
 
 export const presentCommonUser = (commonUser: CommonUserEntity): UserResponse => {
   return {
-    id: commonUser.getId(),
+    ...presentUserBase(commonUser),
     username: commonUser.getUsername(),
-    firstName: commonUser.getFirstName(),
-    lastName: commonUser.getLastName(),
-    email: commonUser.getEmail(),
     identificationNumber: commonUser.getIdentificationNumber(),
     phoneNumber: commonUser.getPhoneNumber(),
     terms: commonUser.getTerms(),
     notifications: commonUser.getNotifications(),
     isActive: commonUser.getIsActive(),
     uid: commonUser.getUid(),
-    role: commonUser.getRole(),
     authProvider: commonUser.getAuthProvider(),
     authType: commonUser.getAuthType(),
     organizationClientId: commonUser.getOrganizationClientId(),
