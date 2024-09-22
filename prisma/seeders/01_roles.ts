cfb922a262c1de20b79ad3ac248b0750f23256f6
Luis Miguel Báez <es.luismiguelbaez@gmail.com>
2024-09-22 12:23:56 -0500

    feat: add seed flow

--- prisma/seeders/01_roles.ts
+++ prisma/seeders/01_roles.ts
@@ -13,11 +13,23 @@ export const seedRoles = async () => {
     { role_name: "GUEST_USER" },
   ];
 
-  const rolePromises = roles.map((role) => {
-    return prisma.role.upsert({
-      where: { role_name: role.role_name },
-      update: {},
-      create: role,
+  const roleNames = roles.map((role) => {
+    return role.role_name;
+  });
+
+  const foundRoles = await prisma.role.findMany({
+    where: { role_name: { in: roleNames } },
+  });
+
+  const rolesToCreate = roles.filter((role) => {
+    return !foundRoles.some((foundRole) => {
+      return foundRole.role_name === role.role_name;
+    });
+  });
+
+  const rolePromises = rolesToCreate.map((role) => {
+    return prisma.role.create({
+      data: { role_name: role.role_name },
     });
   });
 
