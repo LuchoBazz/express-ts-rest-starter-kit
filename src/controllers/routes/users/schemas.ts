f362899be8565e8ea210017370d345c15db87bd0
Luis Miguel Báez <es.luismiguelbaez@gmail.com>
2024-09-22 11:21:43 -0500

    feat: add permission flow

--- src/controllers/routes/users/schemas.ts
+++ src/controllers/routes/users/schemas.ts
@@ -25,3 +25,29 @@ export const roleSchema = checkSchema({
     },
   },
 });
+
+export const permissionSchema = checkSchema({
+  name: {
+    in: ["body"],
+    isEmpty: {
+      options: { ignore_whitespace: true },
+      negated: true,
+      errorMessage: "Missing name.",
+    },
+    optional: {
+      options: {
+        nullable: false,
+      },
+    },
+    exists: {
+      options: {
+        checkFalsy: true,
+      },
+      errorMessage: "name can not be null.",
+      bail: true,
+    },
+    isString: {
+      errorMessage: "name is not a string.",
+    },
+  },
+});
