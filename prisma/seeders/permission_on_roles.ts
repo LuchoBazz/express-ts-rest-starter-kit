297a4333ec753b6360bfa2ae10df953834b43119
Luis Miguel Báez <es.luismiguelbaez@gmail.com>
2024-09-22 13:11:31 -0500

    feat: add seed flow

--- prisma/seeders/permission_on_roles.ts
+++ prisma/seeders/permission_on_roles.ts
@@ -1,5 +1,4 @@
 import { PrismaClient } from "@prisma/client";
-
 import { PermissionsValues } from "../../src/entities/users/authentication.enum";
 
 const prisma = new PrismaClient();
@@ -8,7 +7,7 @@ export const seedPermissionOnRoles = async () => {
   console.log("START_SEEDING_PERMISSION_ON_ROLES");
 
   const permissionOnRoles = {
-    SUPER_ADMIN: [PermissionsValues.CREATE_FEATURE_FLAG],
+    SUPER_ADMIN: [PermissionsValues.UPDATE_CONFIGURATION],
     INTERNAL_ADMIN: [PermissionsValues.CREATE_CONFIGURATION],
     EXTERNAL_ADMIN: [
       PermissionsValues.CREATE_FEATURE_FLAG,
@@ -23,43 +22,21 @@ export const seedPermissionOnRoles = async () => {
     GUEST_USER: [PermissionsValues.GUEST_USER],
   };
 
-  const foundPermissions = await prisma.permissionsOnRoles.findMany({
-    where: {
-      OR: Object.entries(permissionOnRoles).flatMap(([role, permissions]) => {
-        return permissions.map((permission) => {
-          return {
+  const rolePromises = Object.entries(permissionOnRoles).flatMap(([role, permissions]) => {
+    return permissions.map((permission) => {
+      return prisma.permissionsOnRoles.upsert({
+        where: {
+          permissions_on_roles_role_name_permissions_on_roles_permission_name: {
             permissions_on_roles_role_name: role,
             permissions_on_roles_permission_name: permission,
-          };
-        });
-      }),
-    },
-  });
-
-  const rolesToCreate = Object.entries(permissionOnRoles).flatMap(([role, permissions]) => {
-    return permissions
-      .filter((permission) => {
-        return !foundPermissions.some((foundPermission) => {
-          return (
-            foundPermission.permissions_on_roles_role_name === role &&
-            foundPermission.permissions_on_roles_permission_name === permission.toString()
-          );
-        });
-      })
-      .map((permission) => {
-        return {
+          },
+        },
+        update: {},
+        create: {
           permissions_on_roles_role_name: role,
           permissions_on_roles_permission_name: permission,
-        };
+        },
       });
-  });
-
-  const rolePromises = rolesToCreate.map((permissionOnRole) => {
-    return prisma.permissionsOnRoles.create({
-      data: {
-        permissions_on_roles_role_name: permissionOnRole.permissions_on_roles_role_name,
-        permissions_on_roles_permission_name: permissionOnRole.permissions_on_roles_permission_name,
-      },
     });
   });
 
