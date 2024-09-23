2d0986606f9cb6752fca358f675d5f1d83407415
Luis Miguel Báez <es.luismiguelbaez@gmail.com>
2024-09-23 16:27:56 -0500

    docs: add docusaurus docs

--- src/entities/users/internal_admin_user.entity.ts
+++ src/entities/users/internal_admin_user.entity.ts
@@ -3,8 +3,8 @@ import { UserRole } from "./role.enum";
 import { BaseUserEntity } from "./user_base.entity";
 
 export class InternalAdminUserEntity extends BaseUserEntity {
-  constructor(firstName: string, lastName: string, email: string) {
-    super(firstName, lastName, email, UserRole.INTERNAL_ADMIN);
+  constructor(firstName: string, lastName: string, email: string, clientId: string) {
+    super(firstName, lastName, email, UserRole.INTERNAL_ADMIN, clientId);
   }
 
   public getPermissions(): Promise<PermissionsValues[]> {
