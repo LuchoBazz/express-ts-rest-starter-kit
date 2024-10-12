3fed86fa1da28dc288313678298a34a4f9085816
Luis Miguel Báez <es.luismiguelbaez@gmail.com>
2024-10-12 15:08:55 -0500

    feat: Add Subscription Entities Interactors

---
+++ src/core/interactors/subscriptions/subscription_plan.interactor.ts
@@ -0,0 +1,61 @@
+import { PrismaClient } from "@prisma/client";
+
+import { ErrorMessage } from "../../../adapters/api/errors/errors.enum";
+import { NotFoundError } from "../../../adapters/api/errors/not_found.error";
+import { onSession } from "../../../infrastructure/database/prisma";
+import { SubscriptionPlanEntity } from "../../entities/subscriptions/subscription_plan.entity";
+import {
+  createSubscriptionPlanService,
+  deleteSubscriptionPlanService,
+  findSubscriptionPlanService,
+  updateSubscriptionPlanService,
+} from "../../services/subscriptions/subscription_plan.service";
+import {
+  SubscriptionPlanSearchCriteriaInput,
+  UpdateSubscriptionPlanInput,
+} from "../../types/subscriptions/subscription_plan.types";
+
+export const findSubscriptionPlanInteractor = async (
+  searchCriteria: SubscriptionPlanSearchCriteriaInput,
+): Promise<SubscriptionPlanEntity> => {
+  const subscriptionPlanFound = await onSession(async (client: PrismaClient) => {
+    return findSubscriptionPlanService(client, searchCriteria);
+  });
+
+  if (!subscriptionPlanFound) {
+    throw new NotFoundError(ErrorMessage.SUBSCRIPTION_PLAN_NOT_FOUND);
+  }
+
+  return subscriptionPlanFound;
+};
+
+export const createSubscriptionPlanInteractor = async (
+  subscriptionPlan: SubscriptionPlanEntity,
+): Promise<SubscriptionPlanEntity> => {
+  const subscriptionPlanCreated = await onSession((client: PrismaClient) => {
+    return createSubscriptionPlanService(client, subscriptionPlan);
+  });
+
+  return subscriptionPlanCreated;
+};
+
+export const updateSubscriptionPlanInteractor = async (
+  searchCriteria: SubscriptionPlanSearchCriteriaInput,
+  subscriptionPlan: UpdateSubscriptionPlanInput,
+): Promise<SubscriptionPlanEntity> => {
+  const subscriptionPlanUpdated = await onSession((client: PrismaClient) => {
+    return updateSubscriptionPlanService(client, searchCriteria, subscriptionPlan);
+  });
+
+  return subscriptionPlanUpdated;
+};
+
+export const deleteSubscriptionPlanInteractor = async (
+  searchCriteria: SubscriptionPlanSearchCriteriaInput,
+): Promise<SubscriptionPlanEntity> => {
+  const subscriptionPlanDeleted = await onSession((client: PrismaClient) => {
+    return deleteSubscriptionPlanService(client, searchCriteria);
+  });
+
+  return subscriptionPlanDeleted;
+};
---
+++ tests/unit/core/interactors/subscriptions/subscription_plan.interactor.test.ts
@@ -0,0 +1,108 @@
+const subscriptionPlanServiceMock = jest.fn();
+
+import { SubscriptionPlanEntity } from "../../../../../src/core/entities/subscriptions/subscription_plan.entity";
+import {
+  createSubscriptionPlanInteractor,
+  deleteSubscriptionPlanInteractor,
+  findSubscriptionPlanInteractor,
+  updateSubscriptionPlanInteractor,
+} from "../../../../../src/core/interactors/subscriptions/subscription_plan.interactor";
+import { genRandomSubscriptionPlan } from "../../../../mocks/subscriptions/subscription_plan.mock";
+
+const disconnectMock = jest.fn();
+
+jest.mock("../../../../../src/core/services/subscriptions/subscription_plan.service", () => {
+  return {
+    findSubscriptionPlanService: subscriptionPlanServiceMock,
+    createSubscriptionPlanService: subscriptionPlanServiceMock,
+    updateSubscriptionPlanService: subscriptionPlanServiceMock,
+    deleteSubscriptionPlanService: subscriptionPlanServiceMock,
+  };
+});
+
+jest.mock("@prisma/client", () => {
+  return {
+    PrismaClient: jest.fn(() => {
+      return {
+        $disconnect: disconnectMock,
+      };
+    }),
+  };
+});
+
+describe("Given a createSubscriptionPlanInteractor", () => {
+  let subscriptionPlan: SubscriptionPlanEntity;
+
+  beforeEach(() => {
+    const subscriptionPlanPrisma = genRandomSubscriptionPlan();
+    subscriptionPlan = SubscriptionPlanEntity.fromPrisma(subscriptionPlanPrisma);
+
+    subscriptionPlanServiceMock.mockImplementation(() => {
+      return Promise.resolve(subscriptionPlan);
+    });
+  });
+
+  afterEach(() => {
+    subscriptionPlanServiceMock.mockClear();
+    disconnectMock.mockClear();
+  });
+
+  // TODO: Add test for tests nullable case
+  it("should get subscription plan successfully", async () => {
+    const subPlanFound = await findSubscriptionPlanInteractor({
+      id: subscriptionPlan.getId(),
+      clientId: subscriptionPlan.getOrganizationClientId(),
+    });
+
+    expect(subPlanFound).toEqual(subscriptionPlan);
+    expect(subscriptionPlanServiceMock).toHaveBeenCalledTimes(1);
+    expect(disconnectMock).toHaveBeenCalledTimes(1);
+  });
+
+  it("should create subscription plan successfully", async () => {
+    const subPlanCreated = await createSubscriptionPlanInteractor(subscriptionPlan);
+
+    expect(subPlanCreated).toEqual(subscriptionPlan);
+    expect(subscriptionPlanServiceMock).toHaveBeenCalledTimes(1);
+    expect(disconnectMock).toHaveBeenCalledTimes(1);
+  });
+
+  it("should update subscription plan successfully", async () => {
+    const subPlanUpdated = await updateSubscriptionPlanInteractor(
+      {
+        id: subscriptionPlan.getId(),
+        clientId: subscriptionPlan.getOrganizationClientId(),
+      },
+      {
+        name: subscriptionPlan.getName(),
+        productId: subscriptionPlan.getProductId(),
+        variants: subscriptionPlan.getVariants(),
+        slug: subscriptionPlan.getSlug(),
+        price: subscriptionPlan.getPrice(),
+        href: subscriptionPlan.getHref(),
+        billingCycle: subscriptionPlan.getBillingCycle(),
+        description: subscriptionPlan.getDescription(),
+        nodeQuota: subscriptionPlan.getNodeQuota(),
+        features: subscriptionPlan.getFeatures(),
+        mostPopular: subscriptionPlan.getMostPopular(),
+        tier: subscriptionPlan.getTier(),
+        isActive: subscriptionPlan.getIsActive(),
+      },
+    );
+
+    expect(subPlanUpdated).toEqual(subscriptionPlan);
+    expect(subscriptionPlanServiceMock).toHaveBeenCalledTimes(1);
+    expect(disconnectMock).toHaveBeenCalledTimes(1);
+  });
+
+  it("should delete subscription plan successfully", async () => {
+    const subPlanDeleted = await deleteSubscriptionPlanInteractor({
+      id: subscriptionPlan.getId(),
+      clientId: subscriptionPlan.getOrganizationClientId(),
+    });
+
+    expect(subPlanDeleted).toEqual(subscriptionPlan);
+    expect(subscriptionPlanServiceMock).toHaveBeenCalledTimes(1);
+    expect(disconnectMock).toHaveBeenCalledTimes(1);
+  });
+});
