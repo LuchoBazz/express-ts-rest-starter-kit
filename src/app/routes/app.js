c60963dd88c7dedd24118928e4332c2372e6455c
LuchoBazz <LuchoBazz@users.noreply.github.com>
2024-10-13 14:23:05 +0000

    deploy: 458bb9a7eba41faa1c43233f76be6c5a6982caf6

--- src/app/routes/app.js
+++ src/app/routes/app.js
@@ -8,6 +8,7 @@ const express_1 = __importDefault(require("express"));
 const morgan_1 = __importDefault(require("morgan"));
 const basics_1 = require("../../adapters/api/middlewares/basics");
 const organization_router_1 = __importDefault(require("./organization.router"));
+const subscription_router_1 = __importDefault(require("./subscription.router"));
 const router = (0, express_1.default)();
 const logFormat = ":date[iso] :remote-addr :remote-user :method :url " +
     "HTTP/:http-version :status :res[content-length] - :response-time ms\n" +
@@ -20,6 +21,7 @@ router.use(express_1.default.urlencoded({ extended: false }));
 router.get("/", basics_1.healthCheck);
 router.get("/health", basics_1.healthCheck);
 router.use("/organizations", organization_router_1.default);
+router.use("/subscriptions", subscription_router_1.default);
 router.use((0, morgan_1.default)(logFormat));
 router.use(basics_1.logRequest);
 router.use(basics_1.notFound);
