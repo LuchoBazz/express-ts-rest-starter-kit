9880250cd249ee2c86fd17d596ae08de8af77fcf
Luis Miguel Báez <es.luismiguelbaez@gmail.com>
2025-05-30 14:16:29 +0200

    feat: Add Supabase Repository and Its Implementation

---
+++ src/core/repositories/authentication/auth/supabase/auth_supabase.repository.ts
@@ -0,0 +1,39 @@
+import { SupabaseClient } from "@supabase/supabase-js";
+
+import { ErrorMessage } from "../../../../../adapters/api/errors/errors.enum";
+import { UnauthorizedError } from "../../../../../adapters/api/errors/unauthorized.error";
+import { SupabaseClientManager } from "../../../../services/authentication/supabase/supabase_config.service";
+import { encodeJSON } from "../../../../shared/utils/encode_json.util";
+import { AuthUser, DeleteUserPayload, ValidateTokenPayload } from "../../../../types/authentication/base.types";
+import { OrganizationsSupabaseAuthEnv } from "../../../../types/authentication/supabase.types";
+import { AuthRepository } from "../auth_repository.interface";
+
+const supabaseConfig = encodeJSON<OrganizationsSupabaseAuthEnv>(process.env.SUPABASE_CREDENTIALS || "{}", {});
+const manager = SupabaseClientManager.getInstance(supabaseConfig);
+
+export const SupabaseAuthRepository: AuthRepository = {
+  async validateToken(payload: ValidateTokenPayload): Promise<AuthUser> {
+    try {
+      const { clientId, accessToken } = payload;
+      const supabase: SupabaseClient = manager.getClient(clientId);
+      const { data } = await supabase.auth.getUser(accessToken);
+      const { user } = data;
+      if (!user) {
+        throw new UnauthorizedError(ErrorMessage.INVALID_JWT_TOKEN);
+      }
+      return { authId: user.id, email: user.email };
+    } catch (error) {
+      throw new UnauthorizedError(ErrorMessage.INVALID_JWT_TOKEN);
+    }
+  },
+  async deleteUser(payload: DeleteUserPayload): Promise<boolean> {
+    try {
+      const { clientId, authId } = payload;
+      const supabase: SupabaseClient = manager.getClient(clientId);
+      const { error } = await supabase.auth.admin.deleteUser(authId);
+      return !error;
+    } catch (error) {
+      throw new UnauthorizedError(ErrorMessage.DELETING_USER_ERROR);
+    }
+  },
+};
