2d75ff7e948dbbe95266a8f80f6d0ca54ee56371
Luis Miguel Báez <es.luismiguelbaez@gmail.com>
2025-06-10 21:49:04 +0200

    feat: Add Interfaces for Structured JWT Payloads

---
+++ src/core/entities/users/jwt_user.entity.ts
@@ -0,0 +1,26 @@
+export interface JwtUserPayload {
+  id: string;
+  auth_id: string;
+  email: string;
+  username: string;
+  first_name: string;
+  last_name: string;
+  client_id: string;
+  role: string;
+  auth_provider: string;
+  auth_type: string;
+}
+
+export interface JwtDecodedPayload {
+  user: JwtUserPayload;
+  iat: number;
+  iss: string;
+  aud: string[];
+}
+
+export interface JwtPayload {
+  user: string;
+  iat: number;
+  iss: string;
+  aud: string[];
+}
