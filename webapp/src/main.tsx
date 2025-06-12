521c9a663ad8d61a40c12f14282357add62602ee
Luis Miguel Báez <es.luismiguelbaez@gmail.com>
2025-06-12 16:43:30 +0200

    feat: Implement React Router for Enhanced Navigation

--- webapp/src/main.tsx
+++ webapp/src/main.tsx
@@ -1,12 +1,15 @@
 import "./index.css";
 
-import { StrictMode } from "react";
+import React from "react";
 import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 
-import App from "./App.tsx";
+import App from "./App";
 
-createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+createRoot(document.getElementById("root") as HTMLElement).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>,
 );
