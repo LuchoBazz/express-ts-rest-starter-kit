f82be69ea70e0708629b6fa48c9ecb8f5560124c
Luis Miguel Báez <es.luismiguelbaez@gmail.com>
2025-06-12 16:15:23 +0200

    feat: Add SignIn Component in React.js

--- webapp/src/App.tsx
+++ webapp/src/App.tsx
@@ -1,41 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+
+import SignIn from "./components/SignIn/SignIn";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-
-      <div className="flex justify-center items-center min-h-screen bg-gray-100">
-        <button className="bg-blue-500 text-white px-6 py-2 rounded-lg shadow-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-400">
-          Click Me
-        </button>
-      </div>
-    </>
-  )
+    <div className="min-h-screen bg-gray-100 text-gray-900 flex justify-center">
+      <SignIn />
+    </div>
+  );
 }
 
-export default App
+export default App;
