8ba849817127d56599aabbaffeb454c93a0b9d71
Luis Miguel Báez <es.luismiguelbaez@gmail.com>
2025-06-15 13:35:12 +0200

    chore: Exclude webapp Folder from the run build Process

--- src/core/repositories/authentication/auth/auth_repository.interface.ts
+++ src/core/repositories/authentication/auth/auth_repository.interface.ts
@@ -1,13 +1,6 @@
-import { JwtUserPayload } from "../../../entities/users/jwt_user.entity";
-import {
-  AuthUser,
-  DeleteUserPayload,
-  UserLoggedInPayload,
-  ValidateTokenPayload,
-} from "../../../types/authentication/base.types";
+import { AuthUser, DeleteUserPayload, ValidateTokenPayload } from "../../../types/authentication/base.types";
 
 export interface AuthRepository {
   validateToken(payload: ValidateTokenPayload): Promise<AuthUser>;
   deleteUser(payload: DeleteUserPayload): Promise<boolean>;
-  userLoggedIn(payload: UserLoggedInPayload): Promise<JwtUserPayload>;
 }
