69244f7a539dda1045ea2641b3fa582604d8fbd4
Luis Miguel Báez <es.luismiguelbaez@gmail.com>
2025-06-15 13:31:39 +0200

    feat: Add TokenAuthRepository with Concrete Implementation Using JWT

--- src/core/repositories/authentication/token/jwt/token_jwt.repository.ts
+++ src/core/repositories/authentication/token/jwt/token_jwt.repository.ts
@@ -5,23 +5,23 @@ import { decrypt } from "../../../../libs/crypto";
 import { UserLoggedInPayload } from "../../../../types/authentication/base.types";
 import { TokenRepository } from "../token_repository.interface";
 
-const JWT_SECRET = "YOUR_JWT_TOKEN_HERE"; // TODO: Add env
+const JWT_SECRET = "YOUR_JWT_TOKEN_HERE"; // TODO: Move to environment variables for security
 
 export const JwtTokenRepository: TokenRepository = {
   decode(clientId: string, token: string): Promise<UserLoggedInPayload> {
     const decoded = jwt.verify(token, JWT_SECRET) as JwtAuthPayload | null;
-    const invalidTokenResponse = { clientId, jwtToken: null };
+    const invalidTokenResponse = { clientId, jwtDecoded: null };
     if (!decoded?.serialized_user) {
       return Promise.resolve(invalidTokenResponse);
     }
     const deserializedUser = decrypt(decoded.serialized_user);
     const user = JSON.parse(deserializedUser) as JwtUserPayload;
 
-    const jwtToken: JwtDecodedPayload = { ...decoded, user };
+    const jwtDecoded: JwtDecodedPayload = { ...decoded, user };
 
     if (user.client_id !== clientId) {
       return Promise.resolve(invalidTokenResponse);
     }
-    return Promise.resolve({ clientId, jwtToken });
+    return Promise.resolve({ clientId, jwtDecoded });
   },
 };
