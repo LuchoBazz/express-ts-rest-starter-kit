310525ab9d5cdedd53151c29c3db4aababee554f
Luis Miguel Báez <es.luismiguelbaez@gmail.com>
2025-06-17 17:00:09 +0200

    feat: Add Environment Variable for JWT Secret Key

--- src/core/repositories/authentication/token/jwt/token_jwt.repository.ts
+++ src/core/repositories/authentication/token/jwt/token_jwt.repository.ts
@@ -7,7 +7,7 @@ import { decrypt } from "../../../../libs/crypto";
 import { UserLoggedInPayload } from "../../../../types/authentication/base.types";
 import { TokenRepository } from "../token_repository.interface";
 
-const JWT_SECRET = "YOUR_JWT_TOKEN_HERE"; // TODO: Move to environment variables for security
+const JWT_SECRET = process.env.TOKEN_JWT_SECRET_KEY ?? "TOKEN_JWT_SECRET_KEY";
 
 export const JwtTokenRepository: TokenRepository = {
   decode(clientId: string, token: string): Promise<UserLoggedInPayload> {
