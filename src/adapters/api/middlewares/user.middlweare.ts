db6b2c0802d9f4e8241a93f8cec2f0029c888e14
Luis Miguel Báez <es.luismiguelbaez@gmail.com>
2025-06-19 15:06:46 +0200

    housekeeping: Refactor to Send client-id via Headers Instead of URL Path 6/N

--- src/adapters/api/middlewares/user.middlweare.ts
+++ src/adapters/api/middlewares/user.middlweare.ts
@@ -3,14 +3,15 @@ import { NextFunction, Request, Response } from "express";
 import { AuthProvider, AuthType, CommonUserEntity } from "../../../core/entities/users/common_user.entity";
 import { GuestUserEntity } from "../../../core/entities/users/guest_user.entity";
 import { getTokenRepository } from "../../../core/repositories/authentication/token";
+import { getClientIdFromHeaders } from "../../../core/shared/utils/router.util";
 
 export const addUserToRequestMiddleware = async (request: Request, _response: Response, next: NextFunction) => {
   const authorization = (request.headers.Authorization ?? "").toString();
   const token = authorization.replace("Bearer ", "");
+  const clientId = getClientIdFromHeaders(request.headers);
   const tokenRepository = getTokenRepository();
-  const tokenDecoded = await tokenRepository.decode("", token);
-
-  const { clientId, jwtDecoded } = tokenDecoded;
+  const tokenDecoded = await tokenRepository.decode(clientId, token);
+  const { jwtDecoded } = tokenDecoded;
 
   if (!jwtDecoded) {
     request.user = new GuestUserEntity(clientId);
@@ -20,23 +21,21 @@ export const addUserToRequestMiddleware = async (request: Request, _response: Re
   const userDecoded = jwtDecoded.user;
 
   // TODO: Replace placeholder values with actual user information extracted from the token
-  request.user = authorization
-    ? new CommonUserEntity(
-        userDecoded.username,
-        userDecoded.first_name,
-        userDecoded.last_name,
-        userDecoded.email,
-        null,
-        null,
-        true,
-        true,
-        true,
-        "",
-        userDecoded.auth_provider as AuthProvider,
-        userDecoded.auth_type as AuthType,
-        clientId,
-      )
-    : undefined;
+  request.user = new CommonUserEntity(
+    userDecoded.username,
+    userDecoded.first_name,
+    userDecoded.last_name,
+    userDecoded.email,
+    null,
+    null,
+    true,
+    true,
+    true,
+    "",
+    userDecoded.auth_provider as AuthProvider,
+    userDecoded.auth_type as AuthType,
+    clientId,
+  );
 
   next();
 };
