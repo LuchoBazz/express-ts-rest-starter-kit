9cfebe33e280eff40e869123032ea7a6f5fae60a
Luis Miguel Báez <es.luismiguelbaez@gmail.com>
2025-07-15 23:37:32 +0200

    feat: Add UpdateUserInteractor to Handle User Update Logic

---
+++ src/core/interactors/users/users.interactor.ts
@@ -0,0 +1,14 @@
+import { PrismaClient } from "@prisma/client";
+
+import { onSession } from "../../../infrastructure/database/prisma";
+import { StandardUserEntity } from "../../entities/users/a_standard_user.entity";
+import { getUserRepository } from "../../repositories/users/users";
+import { UpdateUserInput } from "../../types/users/user.types";
+
+export const updateUsersInteractor = async (user: UpdateUserInput): Promise<StandardUserEntity> => {
+  const userRepository = getUserRepository();
+
+  const subscriptionUpdated = await onSession((client: PrismaClient) => userRepository.update(client, user));
+
+  return subscriptionUpdated;
+};
