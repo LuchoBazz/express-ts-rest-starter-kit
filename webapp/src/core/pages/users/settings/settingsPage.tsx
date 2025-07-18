896f977552e1e1cba0dce05de65988da8f45ddd6
Luis Miguel Báez <es.luismiguelbaez@gmail.com>
2025-07-18 22:06:16 +0200

    feat: Implement Logic to Update User Information in the Database

--- webapp/src/core/pages/users/settings/settingsPage.tsx
+++ webapp/src/core/pages/users/settings/settingsPage.tsx
@@ -1,8 +1,54 @@
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+
 import MainHeader from "../../../../components/MainHeader/MainHeader";
+import useUpdateUser from "../../../hooks/update-user.hook";
+import useUserLoggedIn from "../../../hooks/user-loggued-in.hook";
+import type { StandardUser } from "../../../entities/standard_user.entity";
 
 // Refence: https://tailwindcss.com/plus/ui-blocks/application-ui/forms/form-layouts
 
 const SettingsPage = () => {
+  const { updateUser } = useUpdateUser();
+  const navigate = useNavigate();
+
+  const { fetchUser } = useUserLoggedIn();
+  const token = localStorage.getItem("token");
+
+  const [email, setEmail] = useState<string>("N/A");
+  const [firstName, setFirstName] = useState<string>("N/A");
+  const [lastName, setLastName] = useState<string>("N/A");
+  const [username, setUserName] = useState<string>("N/A");
+
+  const handleChangeFirstName = (e: React.ChangeEvent<HTMLInputElement>): void => {
+    setFirstName(e.target.value);
+  };
+
+  const handleChangeLastName = (e: React.ChangeEvent<HTMLInputElement>): void => {
+    setLastName(e.target.value);
+  };
+
+  const handleSave = async () => {
+    await updateUser({ email, first_name: firstName, last_name: lastName });
+    // TODO: Refresh Token
+  };
+
+  const handleCancel = () => {
+    navigate("/");
+  };
+
+  const getUser = async (): Promise<void> => {
+    const fetchedUser = await fetchUser(token!);
+    setEmail(fetchedUser?.email ?? "N/A");
+    setFirstName(fetchedUser?.first_name ?? "N/A");
+    setLastName(fetchedUser?.last_name ?? "N/A");
+    setUserName(fetchedUser?.username ?? "N/A");
+  };
+
+  useEffect(() => {
+    getUser();
+  }, []);
+
   return (
     <div className="flex gap-8 h-screen overflow-hidden">
       <div className="w-1/4">
@@ -31,7 +77,7 @@ const SettingsPage = () => {
                       <input
                         id="username"
                         name="username"
-                        value="dostoevsky"
+                        value={username}
                         type="text"
                         disabled
                         placeholder="username"
@@ -56,10 +102,11 @@ const SettingsPage = () => {
                     <input
                       id="first-name"
                       name="first-name"
-                      value="Fyodor"
+                      value={firstName}
                       type="text"
                       autoComplete="given-name"
                       className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 border border-gray-300 placeholder:text-gray-400 focus:border-indigo-600 focus:ring-1 focus:ring-indigo-600 sm:text-sm/6"
+                      onChange={handleChangeFirstName}
                     />
                   </div>
                 </div>
@@ -72,10 +119,11 @@ const SettingsPage = () => {
                     <input
                       id="last-name"
                       name="last-name"
-                      value="Dostoevsky"
+                      value={lastName}
                       type="text"
                       autoComplete="family-name"
                       className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 border border-gray-300 placeholder:text-gray-400 focus:border-indigo-600 focus:ring-1 focus:ring-indigo-600 sm:text-sm/6"
+                      onChange={handleChangeLastName}
                     />
                   </div>
                 </div>
@@ -87,7 +135,7 @@ const SettingsPage = () => {
                   <div className="mt-2">
                     <input
                       id="email"
-                      value="test@test.com"
+                      value={email}
                       name="email"
                       type="email"
                       autoComplete="email"
@@ -288,12 +336,13 @@ const SettingsPage = () => {
             </div>
           </div>
           <div className="mt-6 flex items-center justify-end gap-x-6">
-            <button type="button" className="text-sm/6 font-semibold text-gray-900">
+            <button type="button" className="text-sm/6 font-semibold text-gray-900" onClick={handleCancel}>
               Cancel
             </button>
             <button
               type="submit"
               className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+              onClick={handleSave}
             >
               Save
             </button>
